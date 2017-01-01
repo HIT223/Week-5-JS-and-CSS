c068c570cd316d71b5430294d91395cbba9fe5a6
Jason Carney <jason.jay.carney@gmail.com>
2017-01-01 23:52:23 +0930

    adjust order and add 6th task

--- js/main.js
+++ js/main.js
@@ -25,7 +25,31 @@ pageFooter.innerHTML = pageFooter.innerHTML + ' ' + pageTitle.innerHTML;
 
 
 /*
-* TASK 2: copy the .content_column's into a new row
+* TASK 2: Change heading to red via inline styles and add 20px of left padding
+*/
+
+//pageTitle variable already exists from TASK 1
+pageTitle.style.color = 'red';
+pageTitle.style.paddingLeft = '20px';
+
+// note that this line has no effect as the text-shadow
+// is applied via a stylesheet, not inline
+pageTitle.style.textShadow = null;
+
+
+/*
+* TASK 3: Styles via setAttribute
+*/
+
+var columns = document.querySelectorAll('.content_column');
+
+// Loop through our columns and change their font
+for (var i = 0; i < columns.length; i++) {
+	columns[i].setAttribute('style', 'font-family: Georgia');
+}
+
+/*
+* TASK 4: copy the .content_column's into a new row
 *
 * createElement
 * https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
@@ -40,7 +64,7 @@ pageFooter.innerHTML = pageFooter.innerHTML + ' ' + pageTitle.innerHTML;
 * https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
 */
 
-var columns = document.querySelectorAll('.content_column');
+// columns variable already exists from TASk 2
 
 //create a new div, note it is NOT in our page yet, it's just living in memory
 var newRow = document.createElement('div');
@@ -59,30 +83,36 @@ var card = document.querySelector('.card');
 card.appendChild(newRow);
 
 /*
-* TASK 3: Change heading to red via inline styles and add 20px of left padding
+* TASK 5: Adding a removing classes
+*
+* classList
+* https://developer.mozilla.org/en/docs/Web/API/Element/classList
 */
 
-//pageTitle variable already exists from TASK 1
-pageTitle.style.color = 'red';
-pageTitle.style.paddingLeft = '20px';
+var findableItems = document.querySelectorAll('.find_me');
 
-// note that this line has no effect as the text-shadow is applied via a stylesheet, not inline
-pageTitle.style.textShadow = null;
+for (var i = 0; i < findableItems.length; i++) {
+	findableItems[i].classList.add('found');
+	findableItems[i].classList.remove('find_me');
+}
 
 
 /*
-* TASK 4: Styles via setAttribute
+* TASK 6: Creating an entire stylesheet
+* 
+* CSSStyleSheet
+* https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet
+*
+* David Walsh Article
+* https://davidwalsh.name/add-rules-stylesheets
 */
 
-// columns variable already exists from TASK 2
-// Loop through our columns and change their font
-for (var i = 0; i < columns.length; i++) {
-	/*
-	* Note that only the original columns are changed, this is becuase the new columns did not exist when we set the value of columns
-	* querySelectorAll returns a "non-live" nodeList
-	* - This means it won't update if new items are added to our HTML
-	*
-	* try changing line 41 to a getElementsByClassName function call instead
-	*/
-	columns[i].setAttribute('style', 'font-family: Georgia');
-}
+var styleEl = document.createElement('style');
+
+document.head.appendChild(styleEl);
+
+var sheet = styleEl.sheet; // easy referance
+
+sheet.insertRule('#pageTitle{background-color: red;}', 0);
+sheet.insertRule('#pageTitle{background-color: yellow;}', sheet.cssRules.length);
+sheet.insertRule('p{color: blue;}', sheet.cssRules.length);
