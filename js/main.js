f8669950d85185883757b017d0b7b1c62acd1fa7
Jason Carney <jason.jay.carney@gmail.com>
2017-03-29 22:01:03 +0930

    add comments for each task

--- js/main.js
+++ js/main.js
@@ -5,3 +5,63 @@
 * email: {email}
 * website: {portfolio}
 */
+
+
+
+/*
+* TASK 1: Copy heading into footer
+*
+* innerHTML
+* https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+*/
+
+
+
+
+/*
+* TASK 2: Change heading to red via inline styles and add 20px of left padding
+*/
+
+
+
+
+/*
+* TASK 3: Styles via setAttribute
+*/
+
+
+
+/*
+* TASK 4: copy the .content_column's into a new row
+*
+* createElement
+* https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+*
+* setAttribute
+* https://developer.mozilla.org/en/docs/Web/API/Element/setAttribute
+*
+* outerHTML
+* https://developer.mozilla.org/en-US/docs/Web/API/Element/outerHTML
+*
+* appendChild
+* https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+*/
+
+
+/*
+* TASK 5: Adding a removing classes
+*
+* classList
+* https://developer.mozilla.org/en/docs/Web/API/Element/classList
+*/
+
+
+/*
+* TASK 6: Creating an entire stylesheet
+*
+* CSSStyleSheet
+* https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet
+*
+* David Walsh Article
+* https://davidwalsh.name/add-rules-stylesheets
+*/
